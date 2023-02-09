12d2cb05cc94c146a121a760b615e42f4d49ea7c
Jeffrey <97353049+Delacrobix@users.noreply.github.com>
2023-02-09 14:52:48 -0500

    Removiendo archivos innecesarios

--- java/src/main/java/com/delacrobix/Bingo/dao/ColumnLetterDao.java
+++
@@ -1,7 +0,0 @@
-package com.delacrobix.Bingo.dao;
-
-import com.delacrobix.Bingo.domain.ColumnLetter;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ColumnLetterDao extends CrudRepository<ColumnLetter, Long> {
-}
