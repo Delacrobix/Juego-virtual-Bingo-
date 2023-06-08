040f6ec23f51ee6a68fcfec25f071b3e2ed1999d
Jeffrey <97353049+Delacrobix@users.noreply.github.com>
2023-06-08 13:45:29 -0500

    Cambios en el repositorio del backend dotnet

--- nodejs/src/database/mongodb-connection.js
+++ nodejs/src/database/mongodb-connection.js
@@ -6,6 +6,6 @@ const MONGODB = process.env.MONGODB;
     await mongoose.connect(MONGODB);
     console.log(`Connected to ${MONGODB}`);
   } catch (err) {
-    console.error("ERROR: connecting to Database: " + err.message);
+    console.error('ERROR: connecting to Database: ' + err.message);
   }
 })();
